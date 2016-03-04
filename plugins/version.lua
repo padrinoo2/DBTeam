d07f1d6a6c9f804a66c40496bdb2fda3d5f5bd56
padrinoo2 <padrinoo200@gmail.com>
2016-03-04 13:40:55 +0330

    BehroozTG

--- plugins/version.lua
+++ plugins/version.lua
@@ -15,7 +15,7 @@ do
 
 function run(msg, matches)
   return 'BehroozTG Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/padrinoo2/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@hang_1' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
+  --[[ Checkout https://github.com/padrinoo2/DBTeam
   --GNU GPL v2 license.]]
 end
 
